e171ac587f8d31066d25716b5765e5b1495b1e84
Fırat Cihan <firatcihan18@hotmail.com>
2026-03-02 01:59:58 +0300

    fix: change Toaster position from top-right to top-center

--- components/providers/ToasterProvider.tsx
+++ components/providers/ToasterProvider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 export function ToasterProvider() {
   return (
     <Toaster
-      position="top-right"
+      position="top-center"
       toastOptions={{
         duration: 3000,
         style: {
